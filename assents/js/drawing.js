4a376b49885fb34b53e9020ee78e0666058bf054
Marcos Lucas <marcoslucaspiresmoraessantos@gmail.com>
2024-09-11 20:17:04 -0300

    Add update and erase function

--- assents/js/drawing.js
+++ assents/js/drawing.js
@@ -1,6 +1,9 @@
 const saveButton = document.getElementById('dowload')
+const drawingButton = document.getElementById('drawingMode')
 const clearAllCanvas = document.getElementById('clear')
 const erase = document.getElementById('erase')
+
+let mode = "drawing"
 
 const canvas = document.querySelector('#canvas')
 const ctx = canvas.getContext('2d')
@@ -10,8 +13,10 @@ canvas.height = window.innerHeight * 0.8
 
 let drawing = false
 
+
 const startDrawing = (event) => {
     drawing = true
+
     draw(event)
 }
 
@@ -23,19 +28,56 @@ const stopDrawing = () => {
 const draw = (event) => {
     if(!drawing) return
 
-    ctx.lineWidht = 5
-    ctx.lineCap = 'round'
-    ctx.strokeStyle = '#000'
-
     const rect = canvas.getBoundingClientRect()
     const x = event.clientX - rect.left
     const y = event.clientY - rect.top
 
-    ctx.lineTo(x, y)
-    ctx.stroke()
-    ctx.beginPath()
-    ctx.moveTo(x, y)
+    if(mode == "drawing"){
+        ctx.lineWidth = 5
+        ctx.lineCap = 'round'
+        ctx.strokeStyle = '#000'
+    
+        ctx.lineTo(x, y)
+        ctx.stroke()
+        ctx.beginPath()
+        ctx.moveTo(x, y)
+    }else if(mode == "erase"){
+        ctx.lineWidth = 5
+        ctx.lineCap = 'round'
+        ctx.strokeStyle = '#fff'
+    
+        ctx.lineTo(x, y)
+        ctx.stroke()
+        ctx.beginPath()
+        ctx.moveTo(x, y)
+    }
 }
+
+ function selectStyle() {
+    if(mode == "drawing"){
+        drawingButton.style.border = '5px solid #b7ffa1'
+    }else {
+        drawingButton.style.border = 'none'
+    }
+    
+    if(mode == "erase"){
+        erase.style.border = '5px solid #b7ffa1'
+    }else {
+        erase.style.border = 'none'
+    }
+}
+
+drawingButton.addEventListener('click', () => {
+    mode = "drawing"
+    selectStyle()
+})
+
+erase.addEventListener('click', () => {
+    mode = "erase"
+    selectStyle()
+})
+
+selectStyle()
 
 canvas.addEventListener('mousedown', startDrawing)
 canvas.addEventListener('mousemove', draw)
